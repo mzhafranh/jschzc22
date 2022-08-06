f50a2287fd9bc888552e6783819b473d03283bb7
mzhafranh <16520139@std.stei.itb.ac.id>
2022-08-06 16:52:30 +0700

    c22

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+module.exports = function (db) {
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+ return router;
+}
